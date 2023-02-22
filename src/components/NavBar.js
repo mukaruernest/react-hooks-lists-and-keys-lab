341644550b1887621d6f29e235ed74d2d662ed22
mukaruernest <mukaruernesto@gmail.com>
2023-02-22 14:51:20 +0300

    listandkeyssubmit

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,9 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>
+    {links.map((string)=><a href={string}>{string}</a>)}
+  </nav>;
 }
 
 export default NavBar;
